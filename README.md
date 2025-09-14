0e9c1a22dca63a46acc4dda5003ee3440c5d8fb0
Tendai Karuwo <tendaikaruwo96@gmail.com>
2025-09-14 22:06:17 +0100

    start project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ReactConcepts
+
+## How to install React?
+`npx create-react-app my-app` 
+
+Move into that file:
+`cd my-react-app`
+
+Start the terminal:
+`npm start`
---
+++ my-react-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>run</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React hhh
+        </a>
+      
+    </div>
+  );
+}
+
+export default App;
