afb244503864b79b41ff5e4539dfbaccb22a0915
Tendai Karuwo <tendaikaruwo96@gmail.com>
2025-09-15 00:16:08 +0100

    installed typescript and configured it in respect to react project

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactHome from './Components/ReactHome'
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Learning code concepts </h3>
+      <ReactHome />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Components/ReactHome.tsx
@@ -0,0 +1,18 @@
+
+export default function ReactHome() {
+  return (
+    <>
+      <a className="App-link" href="https://reactjs.org" target="_blank">
+        React
+      </a>
+      <h1>What is React?</h1>
+      <h3>React concepts:</h3>
+      <h4>Hooks</h4>
+      <h5>UseState</h5>
+      <h5>useContext</h5>
+      <h5>useRef</h5>
+      <h5>Redux</h5>
+      <h6>Link https://react-redux.js.org/</h6>
+    </>
+  );
+}
