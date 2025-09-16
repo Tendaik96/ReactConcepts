7f4ee11009fdc97732488401767bc71c8a1403ae
Tendai Karuwo <tendaikaruwo96@gmail.com>
2025-09-17 00:14:03 +0100

    defining different state definitions

--- my-react-app/src/Components/ReactHome.tsx
+++ my-react-app/src/Components/ReactHome.tsx
@@ -8,10 +8,15 @@ export default function ReactHome() {
       <h1>What is React?</h1>
       <h3>React concepts:</h3>
       <h4>Hooks</h4>
+      <h5>Hooks relating to state</h5>
       <h5>UseState</h5>
+      <p>useState is used to manage simple state logic</p>
       <h5>useContext</h5>
-      <h5>useRef</h5>
-      <h5>Redux</h5>
+      <p>useContext is used to share state logic across multiple components, this is done to prevent prop drilling when just simply using useState in a component that needs to pass props to multiple children</p>
+      <h5>useReducer</h5>
+      <p>This is used when state logic become complicated in a single component</p>
+      <h5>React-Redux</h5>
+      <p>This is used to manage state globally, especially when looking to increase scalability</p>
       <h6>Link https://react-redux.js.org/</h6>
     </>
   );
